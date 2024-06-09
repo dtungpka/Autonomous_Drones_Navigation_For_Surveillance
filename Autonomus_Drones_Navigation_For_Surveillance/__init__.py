c301b21e7f2ff3e5c801b76fbf841d6b987ac1d2
DuongTung <duongdoantung2k3@gmail.com>
2024-06-10 04:41:49 +0700

    chore: Update max episode steps and reward threshold for Drone-v0 environment

--- Autonomus_Drones_Navigation_For_Surveillance/__init__.py
+++ Autonomus_Drones_Navigation_For_Surveillance/__init__.py
@@ -3,7 +3,7 @@ from gym.envs.registration import register
 register(
     id="Drone-v0",
     entry_point="Autonomus_Drones_Navigation_For_Surveillance.envs:DroneEnv",
-    max_episode_steps=1000,
-    reward_threshold=1000.0,
+    max_episode_steps=20000,
+    reward_threshold=10000.0,
 
 )
